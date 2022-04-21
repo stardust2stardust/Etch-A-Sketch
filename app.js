9e320880f2d50ca266cc8316ad860edb5217a548
Jeni Hoyt <ziahrahl@gmail.com>
2022-04-21 13:34:37 -0400

    added default grid

--- app.js
+++ app.js
@@ -2,24 +2,45 @@ const grid = document.querySelector('.grid');
 const size = document.querySelector('#grid-size');
 const okBtn = document.querySelector('#ok');
 
+// default grid is 16 x 16
+for (i = 0; i < 256; i++) {
+    const pixel = document.createElement('div');
+    pixel.classList.add('pixel')
+    pixel.style.width = '43.75'
+    pixel.style.height = '43.75px';
+    grid.append(pixel);
+
+}
+
+
+
 // need to access grid attributes and change with user input
-grid.style["grid-template-columns"] = "repeat(16, 31.25px)";
-grid.style["grid-template-rows"] = "repeat(16, 31.25px)";
 
 
 
+// when user clicks 'ok' button, a new grid will appear with 
+// specified size
 okBtn.addEventListener('click', () => {
-    let newSize = parseInt(size.value);
-    console.log(newSize);
-    let width = 500 / newSize;
-    let height = 500 / newSize;
-    for (i = 0; i < newSize; i++) {
+    // clear the grid (remove children)
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+    let newSize = size.value;
+    newSize = parseInt(newSize);
+
+    // Pixel size
+    let pixelWidth = 700 / newSize;
+    let pixelHeight = 700 / newSize;
+    // Grid size
+    grid.style["grid-template-columns"] = `repeat(${newSize}, ${pixelWidth}px)`;
+    grid.style["grid-template-rows"] = `repeat(${newSize}, ${pixelHeight}px)`;
+    for (i = 0; i < (newSize * newSize); i++) {
         const pixel = document.createElement('div');
         pixel.classList.add('pixel')
-        pixel.style.width = `${width}px`;
-        pixel.style.height = `${height}px`;
+        pixel.style.width = `${pixelWidth}px`;
+        pixel.style.height = `${pixelHeight}px`;
         grid.append(pixel);
-        console.log('pixel added')
+
     }
 
 })
