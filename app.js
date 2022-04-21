175ebf8770ae593ffe200eb9cbc8b5733b79f9ac
Jeni Hoyt <ziahrahl@gmail.com>
2022-04-21 12:49:11 -0400

    added way to change grid dynamically using JS

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const grid = document.querySelector('#grid');
+const grid = document.querySelector('.grid');
 const size = document.querySelector('#grid-size');
 const okBtn = document.querySelector('#ok');
 
+// need to access grid attributes and change with user input
+grid.style["grid-template-columns"] = "repeat(16, 31.25px)";
+grid.style["grid-template-rows"] = "repeat(16, 31.25px)";
 
 
-function makePixels(num) {
-    for (i = 0; i < num; i++) {
+
+okBtn.addEventListener('click', () => {
+    let newSize = parseInt(size.value);
+    console.log(newSize);
+    let width = 500 / newSize;
+    let height = 500 / newSize;
+    for (i = 0; i < newSize; i++) {
         const pixel = document.createElement('div');
         pixel.classList.add('pixel')
         pixel.style.width = `${width}px`;
@@ -13,17 +21,8 @@ function makePixels(num) {
         grid.append(pixel);
         console.log('pixel added')
     }
-}
-
-okBtn.addEventListener('click', () => {
-    let newSize = parseInt(size.value);
-    console.log(newSize);
-    let width = 500 / newSize;
-    let height = 500 / newSize;
-    makePixels(newSize);
 
 })
-
 
 
 
