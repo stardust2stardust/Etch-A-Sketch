4309d31907a65e660349590abd83f1b5b1b4470e
Jeni Hoyt <ziahrahl@gmail.com>
2022-04-23 11:02:15 -0400

    changed layout of content

--- app.js
+++ app.js
@@ -1,36 +1,49 @@
 const grid = document.querySelector('.grid');
 const size = document.querySelector('#grid-size');
 const okBtn = document.querySelector('#ok');
+const startOver = document.querySelector('#startOver');
+const colors = document.querySelector('.colorOptions');
+const black = document.querySelector('#black');
+const grey = document.querySelector('#grey');
+const white = document.querySelector('#white');
+const custom = document.querySelector('#custom');
+
+
 
 // default grid is 16 x 16
 for (i = 0; i < 256; i++) {
     const pixel = document.createElement('div');
     pixel.classList.add('pixel')
-    pixel.style.width = '43.75'
-    pixel.style.height = '43.75px';
+    pixel.style.width = '34.375px';
+    pixel.style.height = '34.375px';
     grid.append(pixel);
-
 }
 
-
-
-// need to access grid attributes and change with user input
-
-
-
-// when user clicks 'ok' button, a new grid will appear with 
-// specified size
-okBtn.addEventListener('click', () => {
-    // clear the grid (remove children)
+// function for clear all divs out of grid box
+function clearGrid() {
     while (grid.firstChild) {
         grid.removeChild(grid.firstChild);
     }
+}
+
+// function for resetting grid back to 16x16
+function resetGrid() {
+    clearGrid();
+    makeGrid(16);
+}
+
+// function for getting new size from user input
+function getNewSize() {
     let newSize = size.value;
     newSize = parseInt(newSize);
+    return newSize
+}
 
+// make the new grid with newSize
+function makeGrid(newSize) {
     // Pixel size
-    let pixelWidth = 700 / newSize;
-    let pixelHeight = 700 / newSize;
+    let pixelWidth = 550 / newSize;
+    let pixelHeight = 550 / newSize;
     // Grid size
     grid.style["grid-template-columns"] = `repeat(${newSize}, ${pixelWidth}px)`;
     grid.style["grid-template-rows"] = `repeat(${newSize}, ${pixelHeight}px)`;
@@ -40,11 +53,32 @@ okBtn.addEventListener('click', () => {
         pixel.style.width = `${pixelWidth}px`;
         pixel.style.height = `${pixelHeight}px`;
         grid.append(pixel);
-
     }
+}
 
-})
+// when user clicks 'ok' button, a new grid will appear with 
+// specified size
+okBtn.addEventListener('click', () => {
+    // clear the grid (remove children)
+    clearGrid();
+    // make new grid
+    makeGrid(getNewSize());
+});
+
+startOver.addEventListener('click', () => {
+    resetGrid();
+});
 
 
+colors.addEventListener('click', () => {
 
+});
 
+const squares = grid.children;
+squares[0].addEventListener('click', () => {
+    changeColor(black);
+});
+
+function changeColor(color) {
+    squares[0].style.backgroundColor = `'${color}'`;
+}
