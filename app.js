f678c6d61a287cb588b14fcde7e440c3423f135e
Jeni Hoyt <ziahrahl@gmail.com>
2022-04-25 15:18:36 -0400

    removed white selection button

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const okBtn = document.querySelector('#ok');
 const startOver = document.querySelector('#startOver');
 const colors = document.querySelector('.colorOptions');
 const black = document.querySelector('#blackButton');
-const white = document.querySelector('#whiteButton');
+const rainbow = document.querySelector('#rainbowButton');
 const custom = document.querySelector('#custom');
 let currentColor = 'blue';
 
@@ -62,25 +62,16 @@ function setToBlack() {
     }
 }
 
-function setToGrey() {
-    if (document.getElementById('greyButton')) {
-        currentColor = 'pixel-grey';
-        console.log('current color should be grey');
-        grid.addEventListener('click', draw);
-    }
-}
-
-function setToWhite() {
-    if (document.getElementById('whiteButton')) {
-        currentColor = 'pixel-white';
-        console.log('current color should be white');
-        grid.addEventListener('click', draw);
-    }
-}
+// function setToRainbow() {
+//     if (document.getElementById('customButton')) {
+//         currentColor = custom.value
+//         console.log(currentColor);
+//         currentColor = `${currentColor}`
+//     }
+// }
 
 function draw() {
     console.log(currentColor)
-
     grid.addEventListener('mouseover', startDrawing);
     grid.addEventListener('click', () => {
         grid.removeEventListener('mouseover', startDrawing);
@@ -89,10 +80,7 @@ function draw() {
 
 function startDrawing(e) {
     console.log('mouseover working');
-    if (e.target.classList.contains('pixel')
-        || e.target.classList.contains('pixel-black')
-        || e.target.classList.contains('pixel-white')) {
-        // e.target.classList.add(`${currentColor}`);
+    if (e.target.classList.contains('pixel')) {
         e.target.classList.add(`${currentColor}`);
     }
 }
@@ -100,48 +88,11 @@ function startDrawing(e) {
 // Add EL for clicking on Black Button
 // Should set currentColor to Black 
 black.addEventListener('click', setToBlack);
-white.addEventListener('click', setToWhite);
+// rainbow.addEventListener('click', setToRainbow);
 
 grid.addEventListener('click', draw);
 
 
-// newColor is changed to appropriate color 
-// change color of pixel when mouse hovers
-// function changeColor(newColor) {
-//     grid.addEventListener('mouseover', function (e) {
-//         console.log('mouseover added');
-//         if (e.target.classList.contains('pixel')) {
-//             e.target.classList.add(`${ newColor }`);
-//         }
-//     });
-// }
-
-
-// // when 'Black' clicked, class will change to .pixel-black
-// black.addEventListener('click', () => {
-//     let newColor = 'pixel-black';
-//     console.log(newColor);
-//     // drawing starts once any pixel is clicked in grid
-//     grid.addEventListener('click', changeColor)
-// });
-
-// // when 'Grey' is clicked, class will change to .pixel-grey
-// grey.addEventListener('click', () => {
-//     let newColor = 'pixel-grey';
-//     console.log(newColor);
-//     grid.addEventListener('click', () => {
-//         changeColor(newColor);
-//     })
-// });
-
-// // when white is clicked, class will change to .pixel-white vb 
-// white.addEventListener('click', () => {
-//     let newColor = 'pixel-white';
-//     console.log(newColor);
-//     grid.addEventListener('click', () => {
-//         changeColor(newColor);
-//     })
-// });
 
 // when user clicks 'ok' button, a new grid will appear with 
 // specified size
@@ -155,6 +106,7 @@ okBtn.addEventListener('click', () => {
 // resets to 16x16 grid
 startOver.addEventListener('click', () => {
     resetGrid();
+    grid.removeEventListener('mouseover', startDrawing);
 });
 
 
